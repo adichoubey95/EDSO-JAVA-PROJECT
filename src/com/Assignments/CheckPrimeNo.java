56fb90e688e76556e79b72e74633f26cfc606618
adichoubey95 <adichoubey95@gmail.com>
2024-06-25 03:21:52 +0530

    Updated check prime no code

--- src/com/Assignments/CheckPrimeNo.java
+++ src/com/Assignments/CheckPrimeNo.java
@@ -4,7 +4,7 @@ public class CheckPrimeNo {
     public static void main(String[] args) {
         int n = 49; // let 49 is a prime no
         boolean prime = true;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i <= n; i++) {
             if (n % i == 0) {
                 prime = false;
                 break;
